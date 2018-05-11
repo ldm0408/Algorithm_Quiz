ad7f8c5c134b9f87ef7adfce8e408d97081856eb
ldm0408 <ldm0408@gmail.com>
2018-05-11 16:47:12 +0900

    파이썬 알고리즘

--- Python_quiz/test_for.py
+++ Python_quiz/test_for.py
@@ -1,14 +1,16 @@
-def evenOrOdd(num):
-    s = ""
-    if num % 2 == 0:
-        s ="even"
-    else:
-        s ="Odd"
+def water_melon(n):
+    string = ''
+    for i in range(1,n+1):
+        if i % 2 != 0:
+            string = string + "수"
+        else:
+            string = string +"박"
 
-    return s
+    return string
+   
 
 
-#아래는 테스트로 출력해 보기 위한 코드입니다.
-print("결과 : " + evenOrOdd(3))
-print("결과 : " + evenOrOdd(2))
+# 실행을 위한 테스트코드입니다.
+print("n이 3인 경우: " + water_melon(9))
+print("n이 4인 경우: " + water_melon(4))
 
